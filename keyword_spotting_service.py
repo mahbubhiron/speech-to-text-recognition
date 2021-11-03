216c15096b5dfbe4bd693da7dd08452d752b1b63
Md. Mahbub hiron <mhalom19@gmail.com>
2021-11-04 04:07:04 +0600

    str

---
+++ keyword_spotting_service.py
@@ -0,0 +1,120 @@
+import librosa
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import wave
+import sys
+from scipy.io import wavfile
+
+SAVED_MODEL_PATH = "model.h5"
+SAMPLES_TO_CONSIDER = 22050
+
+class _Keyword_Spotting_Service:
+    """Singleton class for keyword spotting inference with trained models.
+    :param model: Trained model
+    """
+
+    model = None
+    _mapping =[
+        "down",
+        "go",
+        "left",
+        "no",
+        "off",
+        "on",
+        "right",
+        "stop",
+        "up",
+        "yes"
+    ]
+    _instance = None
+
+
+    def predict(self, file_path):
+        """
+        :param file_path (str): Path to audio file to predict
+        :return predicted_keyword (str): Keyword predicted by the model
+        """
+
+        # extract MFCC
+        MFCCs = self.preprocess(file_path)
+
+        # we need a 4-dim array to feed to the model for prediction: (# samples, # time steps, # coefficients, 1)
+        MFCCs = MFCCs[np.newaxis, ..., np.newaxis]
+
+        # get the predicted label
+        predictions = self.model.predict(MFCCs)
+        predicted_index = np.argmax(predictions)
+        predicted_keyword = self._mapping[predicted_index]
+        return predicted_keyword
+
+
+    def preprocess(self, file_path, num_mfcc=13, n_fft=2048, hop_length=512):
+        """Extract MFCCs from audio file.
+        :param file_path (str): Path of audio file
+        :param num_mfcc (int): # of coefficients to extract
+        :param n_fft (int): Interval we consider to apply STFT. Measured in # of samples
+        :param hop_length (int): Sliding window for STFT. Measured in # of samples
+        :return MFCCs (ndarray): 2-dim array with MFCC data of shape (# time steps, # coefficients)
+        """
+
+        # load audio file
+        signal, sample_rate = librosa.load(file_path)
+
+        if len(signal) >= SAMPLES_TO_CONSIDER:
+            # ensure consistency of the length of the signal
+            signal = signal[:SAMPLES_TO_CONSIDER]
+
+            # extract MFCCs
+            MFCCs = librosa.feature.mfcc(signal, sample_rate, n_mfcc=num_mfcc, n_fft=n_fft,
+                                         hop_length=hop_length)
+        return MFCCs.T
+
+
+def Keyword_Spotting_Service():
+    """Factory function for Keyword_Spotting_Service class.
+    :return _Keyword_Spotting_Service._instance (_Keyword_Spotting_Service):
+    """
+
+    # ensure an instance is created only the first time the factory function is called
+    if _Keyword_Spotting_Service._instance is None:
+        _Keyword_Spotting_Service._instance = _Keyword_Spotting_Service()
+        _Keyword_Spotting_Service.model = tf.keras.models.load_model(SAVED_MODEL_PATH)
+    return _Keyword_Spotting_Service._instance
+
+
+
+
+if __name__ == "__main__":
+
+    # create 2 instances of the keyword spotting service
+    kss = Keyword_Spotting_Service()
+    kss1 = Keyword_Spotting_Service()
+
+    # check that different instances of the keyword spotting service point back to the same object (singleton)
+    assert kss is kss1
+
+    # make a wave form
+
+
+    spf = wave.open("Test\\first_checking_wav.wav", "r")
+
+    # Extract Raw Audio from Wav File
+    signal = spf.readframes(-1)
+    signal = np.fromstring(signal, "Int16")
+
+    # If Stereo
+    if spf.getnchannels() == 2:
+        print("Just mono files")
+        sys.exit(0)
+
+    plt.figure(1)
+    plt.title("Signal Wave...")
+    plt.plot(signal)
+    plt.show()
+
+    # make a prediction
+    keyword1 = kss.predict("Test\\first_checking_wav.wav")
+    #keyword1 = kss.predict("dataset\\down\\ffd2ba2f_nohash_1.wav")
+    #keyword2 = kss.predict(filename)
+    print("Predicted keyword is : " +keyword1)
